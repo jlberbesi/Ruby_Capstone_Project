ca40a0412a60a24a42065a81d63e65ab10f6780b
Luis Emilio Rojas <c4r1oszambrano@gmail.com>
2023-11-11 16:01:14 -0500

    Add, game, genre, label, and music:album code

--- classes/item.rb
+++ classes/item.rb
@@ -2,20 +2,34 @@ require 'securerandom'
 require 'date'
 
 class Item
-  attr_accessor :name, :genre, :author, :source, :label
-  attr_reader :publish_date, :id, :archived
+  attr_accessor :source
+  attr_reader :publish_date, :id, :archived, :label, :genre, :author
 
-  def initialize(name, publish_date, author = nil, archived: false)
-    @name = name
-
+  def initialize(publish_date, author = nil, archived: false)
     raise ArgumentError, 'Invalid publish_date format. Please use YYYY-MM-DD.' unless valid_date_format?(publish_date)
 
     @id = SecureRandom.uuid
     @publish_date = Date.parse(publish_date)
     @author = author
     @archived = archived
+    @genre = nil
   rescue ArgumentError => e
     puts e.message
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_item(self) unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.add_item(self) unless author.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.add_item(self) unless label.items.include?(self)
   end
 
   def valid_date_format?(date)
